9f0390348b466b3f8467271392056ac40fb99822
hehf <2251448458@qq.com>
2024-04-26 18:37:25 +0800

    feat: 首页

---
+++ pages/components/theme.vue
@@ -0,0 +1,57 @@
+<template>
+	<u-switch :class="light ? 'theme-switch-light': 'theme-switch-dark'" active-color="#409eff" inactive-color="#dcdfe6"
+		v-model="light"></u-switch>
+</template>
+
+<script lang="ts" setup>
+	import { ref, watch } from 'vue'
+	const light = ref(true)
+	watch(light, (val) => {
+		const html = document.getElementsByTagName('html')[0];
+		html.setAttribute('class', val ? 'light-theme' : 'dark-theme')
+	})
+</script>
+
+<style lang="scss" scoped>
+	@font-face {
+		font-family: "iconfont";
+		/* Project id  */
+		src: url('/assets/fonts/iconfont.ttf?t=1714124436680') format('truetype');
+	}
+
+
+	.theme-switch-dark {
+		font-family: "iconfont" !important;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		::v-deep .u-switch__node {
+			&:before {
+				content: "\e6b6";
+				position: absolute;
+				left: 2px;
+				font-size: calc(100% - 5px);
+				border-radius: 2px;
+			}
+		}
+	}
+
+	.theme-switch-light {
+		font-family: "iconfont" !important;
+		font-size: 16px;
+		font-style: normal;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		::v-deep .node-class {
+			&:before {
+				content: "\e62e";
+				position: absolute;
+				left: 2px;
+				font-size: calc(100% - 6px);
+				border-radius: 2px;
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,67 +1,29 @@
 <template>
 	<view class="content">
-		<image class="logo" src="@/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-
-		<u-action-sheet :list="list" v-model="show"></u-action-sheet>
-		<u-icon name="photo"></u-icon>
+		<div class="theme-btn">
+			<theme></theme>
+		</div>
+		<div class="svg-lottie">
+			<lottie :src="LottieChat"></lottie>
+		</div>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello',
-				list: [{
-						text: '测试',
-						color: 'blue',
-						fontSize: 28
-					},
-					{
-						text: '111'
-					},
-					{
-						text: '111'
-					}
-				],
-				show: true
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script lang="ts" setup>
+	import { ref } from 'vue'
+	import theme from '../components/theme.vue'
+	import lottie from '../components/lottie.vue'
+	import LottieChat from '../../assets/json/lottie-chat.json';
 </script>
 <style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
+		align-items: left;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		background-color: --bg-color;
+		.theme-btn {
+			padding: 10px 0px 5px 10px;
+		}
 	}
 </style>
