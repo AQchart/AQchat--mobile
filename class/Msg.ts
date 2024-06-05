8c22969c4836bb044d3e43d1f793576efb3a335e
howcode <1051495009@qq.com>
2024-06-05 15:26:32 +0800

    feat：相关类

---
+++ class/Msg.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: howcode 1051495009@qq.com
+ * @Date: 2024-05-07 15:11:20
+ * @LastEditors: howcode 1051495009@qq.com
+ * @LastEditTime: 2024-05-22 11:41:14
+ * @Description: 消息类
+ */
+import User from "./User"
+import MsgTypeEnum from "../enums/MsgTypeEnum"
+import MsgStatusEnum from "../enums/MsgStatusEnum"
+
+export default interface Msg {
+  user?:User;
+  roomId:string;
+  msgId?:string|number;
+  msgType:MsgTypeEnum;
+  msg:string;
+  msgStatus?:MsgStatusEnum;
+  ext?:string;
+}
