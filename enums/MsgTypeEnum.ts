7bdeb45f5b8183edcc847530905995ac2d3a1875
howcode <1051495009@qq.com>
2024-06-05 15:32:45 +0800

    feat：相关枚举

---
+++ enums/MsgTypeEnum.ts
@@ -0,0 +1,16 @@
+enum MsgTypeEnum {
+  // 文本
+  TEXT = 0,
+  // 图片
+  IMAGE = 1,
+  // 录音
+  VOICE = 2,
+  // 视频
+  VIDEO = 3,
+  // 文件
+  FILE = 4,
+  // 提示
+  TIP = 99
+}
+
+export default MsgTypeEnum
